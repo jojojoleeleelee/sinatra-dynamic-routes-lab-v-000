9fa254953c84e3f62dfa7270eda37d6dc13c4bff
Joanne Lee <vernapii@gmail.com>
2018-05-01 15:41:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,13 +8,12 @@ class App < Sinatra::Base
   end
 
   get '/square/:number' do
-    binding.pry
     @square = params[:number].to_i * params[:number].to_i
     "#{@square}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-
+    binding.pry
   end
 
   get '/:operation/:number1/:number2' do
