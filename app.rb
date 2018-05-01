f0fd7a36d8b86a3a9fbb941c6be4f3c1505340b4
Joanne Lee <vernapii@gmail.com>
2018-05-01 16:06:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,8 +19,8 @@ class App < Sinatra::Base
     @repeat.times do
         @repeated << params[:phrase]
     end
-    @repeated.join(" ")
-    "#{@repeated}."
+
+    "#{@repeated.join(" ")}."
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
